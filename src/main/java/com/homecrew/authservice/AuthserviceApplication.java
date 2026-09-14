d0361332584e1c4b6d6facaf9be49f03d7cf93ca
mThanuj <mullagrithanuj0@gmailcom>
2026-09-14 14:43:44 +0530

    init pt2

--- src/main/java/com/homecrew/authservice/AuthserviceApplication.java
+++ src/main/java/com/homecrew/authservice/AuthserviceApplication.java
@@ -1,4 +1,4 @@
-package com.homecrew.userservice;
+package com.homecrew.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
